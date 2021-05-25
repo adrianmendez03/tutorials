f9805961b4719e75bb09c732041aaeb1c4ed3924
Adrian Mendez <adrianmndz328@gmail.com>
2021-05-25 14:26:06 -0500

    TypeScript Enumaration

--- typescript/starting_project/app.js
+++ typescript/starting_project/app.js
@@ -7,8 +7,33 @@
 //     age: 21
 // }
 // INFERED 
+// const person : {
+//     name: string,
+//     age: number,
+//     hobbies: string[],
+//     role: [number, string]
+// } = {
+//     name: "Adrian",
+//     age: 21,
+//     hobbies: ['Cooking', 'Video Games'],
+//     role: [2, 'author']
+// }
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 const person = {
     name: "Adrian",
-    age: 21
+    age: 21,
+    hobbies: ['Cooking', 'Video Games'],
+    role: Role.AUTHOR
 };
-console.log(person);
+let favoriteActivities;
+for (const hobby of person.hobbies) {
+    console.log(hobby);
+}
+if (person.role === Role.AUTHOR) {
+    console.log(' is author');
+}
--- typescript/starting_project/app.ts
+++ typescript/starting_project/app.ts
@@ -8,9 +8,33 @@
 // }
 
 // INFERED 
+// const person : {
+//     name: string,
+//     age: number,
+//     hobbies: string[],
+//     role: [number, string]
+// } = {
+//     name: "Adrian",
+//     age: 21,
+//     hobbies: ['Cooking', 'Video Games'],
+//     role: [2, 'author']
+// }
+
+enum Role { ADMIN, READ_ONLY, AUTHOR}
+
 const person = {
     name: "Adrian",
-    age: 21
+    age: 21,
+    hobbies: ['Cooking', 'Video Games'],
+    role: Role.AUTHOR
 }
 
-console.log(person)
+let favoriteActivities: string[]
+
+for (const hobby of person.hobbies) {
+    console.log(hobby)
+}
+
+if (person.role === Role.AUTHOR) {
+    console.log(' is author')
+} 
